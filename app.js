6c2a3fc706355cf118da286f54c4c5e9198ee4f4
Rassemus <Rassemus@users.noreply.github.com>
2020-09-30 21:42:09 +0300

    input added

--- app.js
+++ app.js
@@ -3,18 +3,26 @@ const body = document.querySelector('body');
 const header = document.querySelector('#header');
 const weatherButton = document.querySelector('#weather__button');
 const time = document.querySelector('#time');
+const name = document.querySelector('#name');
+const nameoutput = document.querySelector('#outputname');
 
 function betterWeather() {
   rain.style.background = 'none';
   body.style.background =
     'rgba(255, 255, 255)' + ' url("./images/metsä.jpg")' + 'no-repeat';
   body.style.backgroundSize = 'cover';
-  header.innerHTML = 'Today is a great day!';
+  // nameoutput.innerHTML = name.value;
+  // nameoutput.style.fontSize = '7em';
+  // nameoutput.style.color = '#00ffff';
+  name.style.display = 'none';
   header.style.fontSize = '7em';
   header.style.color = '#00ffff';
   weatherButton.style.display = 'none';
   time.style.color = 'orange';
-  WhatTimeIsis();
+  WhatTimeIsIt();
+}
+if (startTime() < 22) {
+  console.log('on alle 22');
 }
 
 function checkTime(i) {
@@ -31,9 +39,24 @@ function startTime() {
   let s = today.getSeconds();
   m = checkTime(m);
   s = checkTime(s);
+
   time.innerHTML = h + ':' + m + ':' + s;
   t = setTimeout(function () {
     startTime();
   }, 500);
 }
 startTime();
+
+function WhatTimeIsIt() {
+  let now = new Date();
+  let h = now.getHours();
+
+  if (h > 0 && h < 11) {
+    header.innerHTML = name.value + ', This is going to be a great day!';
+  }
+  if (h > 9 && h < 19) {
+    header.innerHTML = name.value + ', Today is a great day!';
+  } else {
+    header.innerHTML = name.value + ', it was a great day!';
+  }
+}
